24a3fcb12c140dfa06d2ca2e477f938d9ff50380
Will Chisholm <william.f.chisholm.iii.20@dartmouth.edu>
2019-04-24 13:26:50 -0400

    finished sa5

--- src/index.js
+++ src/index.js
@@ -1,14 +1,57 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+// eslint-disable-next-line no-unused-vars
 import $ from 'jquery';
 import './style.scss';
+import {
+  BrowserRouter as Router, Route, NavLink, Switch,
+} from 'react-router-dom';
 
-const App = () => <div className="test">All the REACT are belong to us!</div>;
+const About = (props) => {
+  return <div> All there is to know about me </div>;
+};
+const Welcome = (props) => {
+  return <div>Welcome</div>;
+};
+const Nav = (props) => {
+  return (
+    <nav>
+      <ul>
+        <li><NavLink to="/" exact>Home</NavLink></li>
+        <li><NavLink to="/about">About</NavLink></li>
+        <li><NavLink to="/test/id1">test id1</NavLink></li>
+        <li><NavLink to="/test/id2">test id2</NavLink></li>
+      </ul>
+    </nav>
+  );
+};
+const Test = (props) => {
+  return <div> ID: {props.match.params.id} </div>;
+};
+const FallBack = (props) => {
+  return <div>URL Not Found</div>;
+};
+
+const App = (props) => {
+  return (
+    <Router>
+      <div>
+        <Nav />
+        <Switch>
+          <Route exact path="/" component={Welcome} />
+          <Route path="/about" component={About} />
+          <Route exact path="/test/:id" component={Test} />
+          <Route component={FallBack} />
+        </Switch>
+      </div>
+    </Router>
+  );
+};
 
 ReactDOM.render(<App />, document.getElementById('main'));
 
-let num = 0;
-setInterval(() => {
-  num += 1;
-  $('#main').html(`You have been on this page for ${num} seconds!`);
-}, 1000);
+// let num = 0;
+// setInterval(() => {
+//   num += 1;
+//   $('#main').html(`You have been on this page for ${num} seconds!`);
+// }, 1000);
